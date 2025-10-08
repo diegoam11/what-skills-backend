4220a14753830dfd815dd70c877d3417414bd8a9
Ved <ee_dd_uu@outlook.com>
2025-10-08 18:07:32 -0500

    refactor user model

--- src/models/user.py
+++ src/models/user.py
@@ -2,15 +2,21 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserCreate(BaseModel):
-    name: str
-    email: EmailStr
-    password: str
+    first_name: str = Field(..., alias="nombres")
+    last_name: str = Field(..., alias="apellidos")
+    email: EmailStr = Field(..., alias="correo")
+    password: str = Field(..., alias="contraseña")
+    career: str = Field(..., alias="carrera")
+    desired_position: str = Field(..., alias="puesto_laboral_deseado")
 
 
 class UserResponse(BaseModel):
     id: str = Field(..., alias="_id")
-    name: str
-    email: EmailStr
+    first_name: str = Field(..., alias="nombres")
+    last_name: str = Field(..., alias="apellidos")
+    email: EmailStr = Field(..., alias="correo")
+    career: str = Field(..., alias="carrera")
+    desired_position: str = Field(..., alias="puesto_laboral_deseado")
 
     class Config:
         populate_by_name = True
